698ce20dd1e0ee93db780a80adb4d8b48abfb519
Drew Farris <drew.farris@gmail.com>
2020-04-16 17:36:33 -0400

    WIP TermFrequency Query Table

--- warehouse/query-core/src/main/java/datawave/query/tables/term/TermFrequencyQueryTable.java
+++ warehouse/query-core/src/main/java/datawave/query/tables/term/TermFrequencyQueryTable.java
@@ -8,11 +8,13 @@ import java.util.Collections;
 import java.util.Set;
 
 import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.log4j.Logger;
 
+import datawave.ingest.mapreduce.handler.ExtendedDataTypeHandler;
 import datawave.query.QueryParameters;
 import datawave.webservice.common.connection.AccumuloConnectionFactory;
 import datawave.webservice.common.connection.AccumuloConnectionFactory.Priority;
@@ -22,60 +24,114 @@ import datawave.webservice.query.configuration.GenericQueryConfiguration;
 import datawave.webservice.query.logic.BaseQueryLogic;
 import datawave.webservice.query.logic.QueryLogicTransformer;
 
-public class TermFrequencyQueryTable extends BaseQueryLogic<Entry<Key, Value>> {
-
+public class TermFrequencyQueryTable extends BaseQueryLogic<Entry<Key,Value>> {
+    
     protected static final Logger log = ThreadConfigurableLogger.getLogger(TermFrequencyQueryTable.class);
-
+    
     public TermFrequencyQueryTable() {
         super();
         log.debug("Creating TermFrequencyQueryTable: " + System.identityHashCode(this));
     }
-
-    public TermFrequencyQueryTable(BaseQueryLogic<Entry<Key, Value>> other) {
+    
+    public TermFrequencyQueryTable(BaseQueryLogic<Entry<Key,Value>> other) {
         super(other);
         log.debug("Creating TermFrequencyQueryTable: " + System.identityHashCode(this));
     }
-
+    
     @Override
     public Object clone() throws CloneNotSupportedException {
         return new TermFrequencyQueryTable(this);
     }
-
+    
     @Override
     public Priority getConnectionPriority() {
         return AccumuloConnectionFactory.Priority.NORMAL;
     }
-
+    
     @Override
     public Set<String> getExampleQueries() {
         return Collections.emptySet();
     }
-
+    
     @Override
     public Set<String> getOptionalQueryParameters() {
-        return ImmutableSet.of("termfrequency.field.name","termfrequency.match.term","termfrequency.find.term", QueryParameters.CONTENT_VIEW_ALL);
+        return ImmutableSet.of("termfrequency.field.name", "termfrequency.match.term", "termfrequency.find.term", QueryParameters.CONTENT_VIEW_ALL);
     }
-
+    
     @Override
     public Set<String> getRequiredQueryParameters() {
         return Collections.emptySet();
     }
-
+    
     @Override
     public QueryLogicTransformer getTransformer(Query settings) {
         return new TermFrequencyQueryTransformer(settings, markingFunctions);
     }
-
+    
     @Override
-    public GenericQueryConfiguration initialize(Connector connection, Query settings,
-            Set<Authorizations> runtimeQueryAuthorizations) throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+    public GenericQueryConfiguration initialize(Connector connection, Query settings, Set<Authorizations> runtimeQueryAuthorizations) throws Exception {
+        TermFrequencyQueryConfiguration config = new TermFrequencyQueryConfiguration(this, settings);
+        config.setConnector(connection);
+        config.setAuthorizations(auths);
+        
+        int fieldSeparation = settings.query().indexOf(':');
+        String term = null;
+        if (fieldSeparaton > 0) {
+            term = settings.getQuery().substring(fieldSeparation + 1);
+        } else {
+            term = settings.getQuery();
+        }
+        
+        // TODO: Handle other parameters here
+        
+        String[] parts = StringUtils.split(term, '/');
+        
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Query does not specify all necessary parts: " + settings.getQuery()
+                            + ". Should be of the form 'DOCUMENT:shardId/datatype/uid'.");
+        } else {
+            String shardId = parts[0];
+            String datatype = parts[1];
+            String uid = parts[2];
+            
+            log.debug("Received identifier: " + shardId + "," + datatype + "," + uid);
+            
+            String END = PARENT_ONLY;
+            Parameter p = settings.findParameter(QueryParameters.CONTENT_VIEW_ALL);
+            if (p != null && Boolean.parseBoolean(p.getParameterValue())) {
+                END = ALL;
+            }
+            
+            final String tf = ExtendedDataTypeHandler.TERM_FREQUENCY_COLUMN_FAMILY.toString();
+            Key startKey = new Key(shardId, tf, datatype + NULL + uid + NULL);
+            Key endKey = new Key(shardId, tf, datatype + NULL + uid + END);
+            Range r = new Range(startKey, true, endKey, false);
+            
+            config.setRange(r);
+            
+            log.debug("Setting range: " + r);
+        }
+        
+        return config;
     }
-
+    
     @Override
     public void setupQuery(GenericQueryConfiguration configuration) throws Exception {
-        // TODO Auto-generated method stub
-
+        if (!configuration.getClass().getName().equals(TermFrequencyQueryConfiguration.class.getName())) {
+            throw new QueryException("Did not receive a TermFrequencyQueryConfiguration instance");
+        }
+        
+        TermFrequencyQueryConfiguration tfConfig = (TermFrequencyQueryConfiguration) configuration;
+        
+        try {
+            Scanner scanner = QueryScannerHelper.createScanner(tcConfig.getConnector(), tcConfig.getTableName(), tcConfig.getAuthorizations(),
+                            tcConfig.getQuery());
+            scanner.setRange(config.getRange());
+            
+            this.iterator = scanner.iterator();
+            this.scanner = scanner;
+        } catch (TableNotFoundException e) {
+            throw new RuntimeException("Table not found: " + this.getTableName(), e);
+        }
     }
 }
--- warehouse/query-core/src/main/java/datawave/query/transformer/TermFrequencyQueryTransformer.java
+++ warehouse/query-core/src/main/java/datawave/query/transformer/TermFrequencyQueryTransformer.java
@@ -7,31 +7,42 @@ import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.curator.shaded.com.google.common.collect.ImmutableList;
+import org.apache.lucene.document.Field;
 
 import datawave.ingest.data.RawRecordContainer;
 import datawave.marking.MarkingFunctions;
+import datawave.query.search.Term;
+import datawave.query.table.parser.TermFrequencyKeyValueFactory.TermFrequencyKeyValue;
 import datawave.webservice.query.Query;
 import datawave.webservice.query.exception.EmptyObjectException;
 import datawave.webservice.query.logic.BaseQueryLogicTransformer;
 import datawave.webservice.result.BaseQueryResponse;
 
-public class TermFrequencyQueryTransformer extends BaseQueryLogicTransformer<Entry<Key, Value>, RawRecordContainer> {
-
+public class TermFrequencyQueryTransformer extends BaseQueryLogicTransformer<Entry<Key,Value>,RawRecordContainer> {
+    
     private Query query = null;
     private Authorizations auths = null;
-
+    
     public TermFrequencyQueryTransformer(Query query, MarkingFunctions markingFunctions) {
         super(markingFunctions);
         this.query = query;
         this.auths = new Authorizations(StringUtils.split(this.query.getQueryAuthorizations(), ','));
     }
-
+    
     @Override
     public BaseQueryResponse createResponse(List<Object> resultList) {
-        // TODO Auto-generated method stub
-        return null;
+        EventQueryResponse response = new EventQueryResponse();
+        List<EventBase> eventList = new ArrayList<>();
+        for (Object o : resultList) {
+            RawRecordContainer result = (RawRecordContainer) o;
+            eventList.add(result);
+        }
+        response.setEvents(eventList);
+        response.setReturnedEvents(new Long(eventList.size()));
+        return response;
     }
-
+    
     @Override
     public RawRecordContainer transform(Entry<Key, Value> input) throws EmptyObjectException {
         if (entry.getKey() == null && entry.getValue() == null) {
@@ -52,7 +63,29 @@ public class TermFrequencyQueryTransformer extends BaseQueryLogicTransformer<Ent
 
         e.setMarkings(tfkv.getMarkings());
         
-    }
+        Metadata m = new Metadata();
+        m.setRow(tfkv.getShardId());
+        m.setDataType(tfkv.getShardId());
+        m.setInternalId(tfkv.getUit());
+        e.setMetadata(m);
 
+        List<Field> fields = ImmutableList.of(
+            createField(tfkv, entry, "FIELD_NAME", tfkv.getFieldName()),
+            createField(tfkv, entry, "FIELD_VALUE", tfkv.getFieldValue()),
+            createField(tfkv, entry, "OFFSET_COUNT", tfkv.getCount()),
+            createField(tfkv, entry, "OFFETS", tfkv.getOffsets().toString())
+        )
+        e.setFields(fields);
+
+        return e;
+    }
     
+    protected Field createField(TermFrequencyKeyValue tfkv, Entry<Key,Value> e, String name, String value) {
+        Field field = new Field();
+        field.setMarkings(tfkv.getMarkings());
+        field.setName(name);
+        field.setValue(value);
+        field.setTimestamp(e.getKey().getTimestamp());
+        return field;
+    }
 }
