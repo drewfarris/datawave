9298e135b2f170ec2c7aae653b9057b69d4be601
Drew Farris <drew.farris@gmail.com>
2025-07-09 16:32:57 +0000

    WIP: segment match validation

--- warehouse/core/src/test/java/datawave/annotation/model/AnnotationTest.java
+++ warehouse/core/src/test/java/datawave/annotation/model/AnnotationTest.java
@@ -7,11 +7,14 @@ import datawave.annotation.protobuf.SegmentValue;
 import datawave.data.hash.UID;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AnnotationTest {
 
@@ -124,6 +127,47 @@ public class AnnotationTest {
     }
 
     public static void assertSegmentsEqual(List<Segment> expected, List<Segment> result) {
-        //TODO: implement this
+        Map<String, Segment> expectedByUID = indexSegments(expected);
+        Set<String> expectedUIDs = expectedByUID.keySet();
+
+        Map<String, Segment> resultByUID = indexSegments(result);
+        Set<String> resultUIDs = resultByUID.keySet();
+
+        List<String> missing = new ArrayList<>(expectedUIDs);
+        missing.removeAll(resultUIDs);
+
+        List<String> extra = new ArrayList<>(resultUIDs);
+        extra.removeAll(expectedUIDs);
+
+        List<String> mismatchMessages = new ArrayList<>();
+        if (!missing.isEmpty()) {
+            mismatchMessages.add("Results are missing expected uids: " + missing);
+        }
+        if (!extra.isEmpty()) {
+            mismatchMessages.add("Results have extra uids: " + extra);
+        }
+        assertTrue("Mismatch in uuids observed: " + mismatchMessages, mismatchMessages.isEmpty());
+
+        
+        List<String> mismatchedSegmentMessages = new ArrayList<>();
+        for (Map.Entry<String, Segment> expectedSegment: expectedByUID.entrySet()) {
+            String expectedKey = expectedSegment.getKey();
+            Segment resultSegment = resultByUID.get(expectedKey);
+            compareSegments(expectedSegment, resultSegment, mismatchedSegmentMessages);
+        }
+
+        assertTrue("Segment mismatches observed: " + mismatchedSegmentMessages, mismatchedSegmentMessages.isEmpty());
+    }
+
+    public static Map<String, Segment> indexSegments(List<Segment> input) {
+        final Map<String, Segment> index = new HashMap<>();
+        for (Segment s: input) {
+            index.put(s.getSegmentId().toString(), s);
+        }
+        return index;
+    }
+
+    public static void compareSegments(String expected, String result, List<String> mismatchedSegmentMessages) {
+
     }
 }
